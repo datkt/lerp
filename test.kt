18fe5b22d2e6e9e2bb83ec492c159c7e7317091c
Joseph Werle <joseph.werle@gmail.com>
2018-10-25 11:30:48 -0400

    test(test.kt): Clean up test

--- test.kt
+++ test.kt
@@ -4,14 +4,12 @@ import lerp.*
 import tape.test
 import tape.Test
 
-class Beep: Throwable()
-
 fun main(args: Array<String>) {
-  test("lerp", fun(t: Test) {
+  test("lerp(Double, Double, Double) -> Double", fun(t: Test) {
     val x = 1.0
     val y = 2.0
     val z = lerp(x, y, x)
-    t.ok(y == z, "y == lerp(x, y, x)")
+    t.equal(y, z, "y == lerp(x, y, x)")
     t.end()
   })
 
